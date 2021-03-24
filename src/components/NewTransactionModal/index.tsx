a998ee35a06a2d7a04a4e1c8e6f1bd3c642e8f0c
atirson <fabiano.oliveira2213@gmail.com>
2021-03-24 08:00:09 -0300

    BUG - Input not follow pattern of modal

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,43 @@
+import Modal from 'react-modal';
+import { Container } from './styles';
+
+interface NewTrasanctionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+export function NewTrasanctionModal({isOpen, onRequestClose}: NewTrasanctionModalProps) {
+  return (
+    <Modal 
+    isOpen={isOpen}
+    onRequestClose={onRequestClose}
+    overlayClassName="react-modal-overlay"
+    className="react-modal-content"
+  >
+    
+    <Container>
+      <h2>Cadastrar transação</h2>
+
+      <input 
+        placeholder="Título" 
+      />
+
+      <input 
+        type="number"
+        placeholder="Valor" 
+      />
+
+      <input 
+        placeholder="Categoria" 
+      />
+
+      <button type="submit">
+        Cadastrar
+      </button>
+
+    </Container>
+    
+  </Modal>
+
+  )
+}
